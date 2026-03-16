787b9510d637fba80265a0a95a3f091c0f386f32
Amit Nandi <mr.amitnandi1@gmail.com>
2026-03-17 02:34:17 +0600

    Add script to disable emojis and embeds in WordPress
    This script disables WordPress emojis and embeds to reduce HTTP requests and improve site speed.

---
+++ disable-emojis-embeds.php
@@ -0,0 +1,34 @@
+<?php
+/**
+ * Disable Emojis and Embeds
+ *
+ * Removes unnecessary WordPress core features that add extra HTTP requests
+ * and bloat to the <head> section, speeding up the site.
+ *
+ * Add to functions.php or custom plugin.
+ */
+function disable_emojis() {
+    remove_action( 'wp_head', 'print_emoji_detection_script', 7 );
+    remove_action( 'wp_print_styles', 'print_emoji_styles' );
+    remove_action( 'admin_print_scripts', 'print_emoji_detection_script' );
+    remove_action( 'admin_print_styles', 'print_emoji_styles' );
+    remove_filter( 'the_content_feed', 'wp_staticize_emoji' );
+    remove_filter( 'comment_text_rss', 'wp_staticize_emoji' );
+    remove_filter( 'wp_mail', 'wp_staticize_emoji_for_email' );
+    add_filter( 'tiny_mce_plugins', 'disable_emojis_tinymce' );
+    // Disable embeds
+    remove_action( 'rest_api_init', 'wp_oembed_register_route' );
+    remove_filter( 'oembed_dataparse', 'wp_filter_oembed_result', 10 );
+    remove_action( 'wp_head', 'wp_oembed_add_discovery_links' );
+    remove_action( 'wp_head', 'wp_oembed_add_host_js' );
+}
+add_action( 'init', 'disable_emojis' );
+
+function disable_emojis_tinymce( $plugins ) {
+    if ( is_array( $plugins ) ) {
+        return array_diff( $plugins, array( 'wpemoji' ) );
+    } else {
+        return array();
+    }
+}
+?>
